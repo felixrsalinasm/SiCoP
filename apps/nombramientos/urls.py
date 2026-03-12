929a6a48d5aa9b030ba4dea7d97d656193949862
Felix Ricardo Salinas Melendez <arxmiv@icloud.com>
2026-03-11 19:07:12 -0600

    feat: CRUD nombramientos y tipos de nombramiento

---
+++ apps/nombramientos/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from .views import (
+    ListaNombramientos, CrearNombramiento, DetalleNombramiento, EditarNombramiento, EliminarNombramiento,
+    ListaTiposNombramiento, CrearTipoNombramiento, EditarTipoNombramiento,
+    AsignarNombramientoProfesor, EliminarNombramientoProfesor,
+    AsignarNombramientoPrograma, EliminarNombramientoPrograma
+)
+
+app_name = 'nombramientos'
+
+urlpatterns = [
+    path('', ListaNombramientos.as_view(), name='lista_nombramientos'),
+    path('nuevo/', CrearNombramiento.as_view(), name='crear_nombramiento'),
+    path('<int:pk>/', DetalleNombramiento.as_view(), name='detalle_nombramiento'),
+    path('<int:pk>/editar/', EditarNombramiento.as_view(), name='editar_nombramiento'),
+    path('<int:pk>/eliminar/', EliminarNombramiento.as_view(), name='eliminar_nombramiento'),
+    
+    path('tipos/', ListaTiposNombramiento.as_view(), name='lista_tipos'),
+    path('tipos/nuevo/', CrearTipoNombramiento.as_view(), name='crear_tipo'),
+    path('tipos/<int:pk>/editar/', EditarTipoNombramiento.as_view(), name='editar_tipo'),
+    
+    path('asignar-profesor/', AsignarNombramientoProfesor.as_view(), name='asignar_profesor'),
+    path('asignar-profesor/<int:pk>/eliminar/', EliminarNombramientoProfesor.as_view(), name='eliminar_nombramiento_profesor'),
+    
+    path('asignar-programa/', AsignarNombramientoPrograma.as_view(), name='asignar_programa'),
+    path('asignar-programa/<int:pk>/eliminar/', EliminarNombramientoPrograma.as_view(), name='eliminar_nombramiento_programa'),
+]
